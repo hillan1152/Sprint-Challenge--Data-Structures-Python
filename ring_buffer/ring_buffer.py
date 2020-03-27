3051ee995f0a102faf698665852560062bea7061
Anthony Hill <hillan1152@gmail.com>
2020-03-27 11:55:37 -0400

    ring buffer plan not working, restarting from scratch

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,4 +1,12 @@
 from doubly_linked_list import DoublyLinkedList
+# UNDERSTAND:
+#   Has a fixed size = capacity
+#       The CAPACITY IS WHAT IS BEING PASSED IN THE RINGBUFFER
+#   When capacity is full & add new element, oldest element is overwritten--- LRU CACHE
+#   Methods:
+#       Append - adds elements to buffer
+#       Get - returns ALL elements in the buffer list in their given order
+#             should not return NONE
 
 
 class RingBuffer:
@@ -6,9 +14,25 @@ class RingBuffer:
         self.capacity = capacity
         self.current = None
         self.storage = DoublyLinkedList()
+    # I CAN USE CURRENT AS A CURSOR OVER THE OLDEST POSSIBLE ITEM, TO REPLACE WHEN IT IS
 
     def append(self, item):
-        pass
+        # if list is empty, add it to the head
+        if len(self.storage) is None:
+            # add to head
+            self.storage.add_to_tail(item)
+            # make it the current item
+            self.current == item
+            print("current item ---> ", item)
+        if len(self.storage) < self.capacity:
+            # add to tail
+            self.storage.add_to_tail(item)
+        # if current = capacity:
+        if len(self.storage) == self.capacity:
+            del self.storage[self.current]
+            item = self.current
+            self.current.next == self.current
+        #       add to head of
 
     def get(self):
         # Note:  This is the only [] allowed
@@ -30,3 +54,10 @@ class ArrayRingBuffer:
 
     def get(self):
         pass
+
+
+r = RingBuffer(4)
+r.append('a')
+r.append('e')
+r.append('i')
+r.append('o')
