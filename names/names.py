afafcba8ccdf89954081697f35d37dc1d085b3e1
Anthony Hill <hillan1152@gmail.com>
2020-03-27 13:11:26 -0400

    narrowed down run time to 2.6 seconds by eliminating a loop

--- names/names.py
+++ names/names.py
@@ -14,13 +14,14 @@ duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    if names_1 in names_2:
+        duplicates.append(name_1)
+
+#   REDUCED THE RUNTIME DOWN TO 2.26 SECONDS
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
